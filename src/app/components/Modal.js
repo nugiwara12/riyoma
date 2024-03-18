319404c42be272b034a7ed1e1a5078b744483901
nugiwara12 <131630658+nugiwara12@users.noreply.github.com>
2024-03-18 10:58:02 +0800

    Update Modal.js

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -18,36 +18,37 @@ const handleClose = (e) => {
             backdrop-blur-sm flex justify-center item-center" id="wrapper" onClick={handleClose}>
 
             <div className="w-[40rem] flex flex-col">
-                {/* <button className="text-white text-xl place-self-end" onClick={() => onClose()}>
-                    <IoIosCloseCircle />
-                </button> */}
+                <button className="text-white text-xl place-self-end" onClick={() => onClose()}>
+                    {/* <IoIosCloseCircle /> */}
+                </button>
                 <div className="bg-white p-2 rounded  border border-black mt-10">
                 
-                <div className="max-w-xxl mx-auto my-10 p-6 bg-white rounded-md shadow-md landscape">
-                    <h2 className="text-2xl font-semibold mb-6 text-center">Register</h2>
-                    <form className="grid grid-cols-2 gap-4">
-                        <div className="mb-4">
-                            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-                            <input type="text" id="username" className="w-full px-3 py-2 border border-gray-500 rounded-md" />
+                <div class="max-w-2xl mx-auto my-10 p-6 bg-white rounded-md shadow-md sm:landscape">
+                    <h2 class="text-2xl font-semibold mb-6 text-center">Register</h2>
+                    <form class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                        <div class="mb-4">
+                            <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username</label>
+                            <input type="text" id="username" class="w-full px-3 py-2 border border-gray-500 rounded-md" />
                         </div>
-                        {/* <div className="mb-4">
-                            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                            <input type="email" id="email" className="w-full px-3 py-2 border  border-gray-500 rounded-md" />
+                        {/* <div class="mb-4">
+                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                            <input type="email" id="email" class="w-full px-3 py-2 border border-gray-500 rounded-md" />
                         </div> */}
-                        <div className="mb-4">
-                            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                            <input type="password" id="password" className="w-full px-3 py-2 border  border-gray-500 rounded-md" />
+                        <div class="mb-4">
+                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                            <input type="password" id="password" class="w-full px-3 py-2 border border-gray-500 rounded-md" />
                         </div>
-                        {/* <div className="mb-6">
-                            <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
-                            <input type="password" id="confirmPassword" className="w-full px-3 py-2 border  border-gray-500 rounded-md" />
+                        {/* <div class="mb-6">
+                            <label for="confirmPassword" class="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
+                            <input type="password" id="confirmPassword" class="w-full px-3 py-2 border border-gray-500 rounded-md" />
                         </div> */}
-                        <div className="col-span-2 flex justify-end">
-                            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mr-3">Register</button>
-                            <button type="button" className="bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-900" onClick={() => onClose()}>Cancel</button>
+                        <div class="flex">
+                            <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mr-3">Register</button>
+                            <button type="button" class="bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-900" onClick={() => onClose()}>Cancel</button>
                         </div>
                     </form>
                 </div>
+
 
                             
                 </div>

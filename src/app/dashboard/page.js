fff6a43eb477c933ad09b67bcfb269fcac63c16a
edgar <edgar>
2024-03-19 17:57:51 +0800

    ha

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -27,9 +27,9 @@ export default function Dashboard() {
         </thead>
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Apple MacBook Pro 17"
-                </th>
+                </td>
                 <td class="px-6 py-4">
                     Silver
                 </td>
@@ -41,9 +41,9 @@ export default function Dashboard() {
                 </td>
             </tr>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Microsoft Surface Pro
-                </th>
+                </td>
                 <td class="px-6 py-4">
                     White
                 </td>
@@ -53,11 +53,11 @@ export default function Dashboard() {
                 <td class="px-6 py-4">
                     $1999
                 </td>
-            </tr>
+            </tr>  
             <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Magic Mouse 2
-                </th>
+                </td>
                 <td class="px-6 py-4">
                     Black
                 </td>
@@ -71,6 +71,7 @@ export default function Dashboard() {
         </tbody>
     </table>
 </div>
+
       </h1>
   );
 }

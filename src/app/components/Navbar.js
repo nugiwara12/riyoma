597a0b1611f25ea17d2f05a6fb03c36fabfe4935
Faocci Layug <faocciworx@gmail.com>
2024-03-19 15:52:23 +0800

    Added Hidden Toggle in Mobile

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { FiMenu } from 'react-icons/fi';
-import { IoIosNotifications } from 'react-icons/io';
-import { IoCloseOutline } from 'react-icons/io5';
+import React, { useState } from "react";
+import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
+import { IoIosNotifications } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 import { MdFlagCircle } from "react-icons/md";
-import Image from 'next/image';
-import clsx from 'clsx';
+import Image from "next/image";
+import clsx from "clsx";
 import { IoMdArrowDropright } from "react-icons/io";
 
 export default function Navbar() {
@@ -20,10 +20,10 @@ export default function Navbar() {
 
   const handleAdminClick = (event) => {
     // Check if the clicked element is IoIosNotifications or MdFlagCircle
-    if (event.target.tagName === 'svg' || event.target.tagName === 'path') {
+    if (event.target.tagName === "svg" || event.target.tagName === "path") {
       return; // Do nothing, don't toggle dropdown
     }
-    toggleDropdown('admin');
+    toggleDropdown("admin");
   };
 
   const handleContactClick = (index) => {
@@ -32,36 +32,36 @@ export default function Navbar() {
 
   const navlinks = [
     {
-      label: 'User',
-      link: '/user'
+      label: "User",
+      link: "/user",
     },
     {
-      label: 'Dashboard',
-      link: '/dashboard'
+      label: "Dashboard",
+      link: "/dashboard",
     },
     {
-      label: 'Men',
-      link: '/men'
+      label: "Men",
+      link: "/men",
     },
     {
-      label: 'About',
-      link: '#'
+      label: "About",
+      link: "#",
     },
     {
-      label: 'Contact',
-      link: '#',
+      label: "Contact",
+      link: "#",
       icon: IoMdArrowDropright,
       dropdown: [
         {
-          label: 'Option 1',
-          link: '#'
+          label: "Option 1",
+          link: "#",
         },
         {
-          label: 'Option 2',
-          link: '#'
-        }
-      ]
-    }
+          label: "Option 2",
+          link: "#",
+        },
+      ],
+    },
   ];
 
   return (
@@ -75,7 +75,7 @@ export default function Navbar() {
               className="text-3xl cursor-pointer lg:hidden"
             />
             {/* logo */}
-            <Link href={'/dashboard'} className="text-4xl font-mono">
+            <Link href={"/dashboard"} className="text-4xl font-mono">
               <Image
                 width={80}
                 height={80}
@@ -95,7 +95,9 @@ export default function Navbar() {
               </a>
               {d.icon && (
                 <d.icon
-                    className={`absolute top-1 left-16 transform w-5 h-4 ${activeDropdown === i && "rotate-90"} transition-all duration-200`}
+                  className={`hidden md:inline absolute top-1 left-16 transform w-5 h-4 ${
+                    activeDropdown === i && "rotate-90"
+                  } transition-all duration-200`}
                 />
               )}
               {d.dropdown && activeDropdown === i && (
@@ -119,8 +121,8 @@ export default function Navbar() {
         {/* sidebar mobile menu */}
         <div
           className={clsx(
-            'fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0 -translate-x-full transition-all',
-            isSideMenuOpen && 'translate-x-0'
+            "fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0 -translate-x-full transition-all",
+            isSideMenuOpen && "translate-x-0"
           )}
         >
           <section className="text-black bg-white flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 w-56 flex">
@@ -139,7 +141,10 @@ export default function Navbar() {
 
         {/* Admin section */}
         <div className="relative">
-          <section className="flex items-center gap-1 cursor-pointer " onClick={handleAdminClick}>
+          <section
+            className="flex items-center gap-1 cursor-pointer "
+            onClick={handleAdminClick}
+          >
             {/* Icon section */}
             <IoIosNotifications className="text-3xl mr-5 cursor-pointer" />
             <MdFlagCircle className="text-3xl mr-5 cursor-pointer" />
@@ -153,23 +158,32 @@ export default function Navbar() {
             <h1>Admin</h1>
             {/* avtar img */}
             {/* Dropdown icon */}
-          <IoMdArrowDropright className={`transform w-4 h-5 ${activeDropdown === 'admin' && "rotate-90"} transition-all duration-200`} />
+            <IoMdArrowDropright
+              className={`transform w-4 h-5 ${
+                activeDropdown === "admin" && "rotate-90"
+              } transition-all duration-200`}
+            />
           </section>
           {/* Dropdown */}
-          {activeDropdown === 'admin' && (
+          {activeDropdown === "admin" && (
             <div className="absolute top-full right-0 mt-4 w-[120px] text-center bg-white border border-gray-200 rounded shadow-lg">
               {/* Dropdown content */}
               <ul>
-                <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">Profile</li>
-                <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">About</li>
+                <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">
+                  Profile
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">
+                  About
+                </li>
                 <Link href="./auth">
-                  <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">Log out</li>
+                  <li className="px-4 py-2 hover:bg-gray-400 cursor-pointer hover:text-sky-700">
+                    Log out
+                  </li>
                 </Link>
               </ul>
             </div>
           )}
-        </div>        
-
+        </div>
       </nav>
       <hr className="" />
     </main>
